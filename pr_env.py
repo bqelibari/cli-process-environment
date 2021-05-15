a140f96c20a90c3ec35267b8da346304e6c10c86
Bajram <bajram.qelibari@googlemail.com>
2021-05-15 02:53:31 +0200

    refactored mainfunction

--- pr_env.py
+++ pr_env.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-import argparse
-import psutil
+from argparse import ArgumentParser, RawDescriptionHelpFormatter, MetavarTypeHelpFormatter
+from psutil import Process, pid_exists
 import sys
 from pathlib import Path
 import subprocess
@@ -8,47 +8,58 @@ import subprocess
 
 def define_parser_with_arguments():
     description_text = "Prints different information about the given <PROCESS_ID> \
-\nor if none given the current process.\n\
-If no option is given it is assumed that\
-all options were given."
+                        \nor if none given the current process.\n\
+                        If no option is given it is assumed that\
+                        all options were given."
 
-    parser = argparse.ArgumentParser(description=description_text,
-                                     formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     usage="%(prog)s [-h|--help] [-p|--pid] [-f|--homefolder] [<PROCESS_ID>]")
+    parser = ArgumentParser(description=description_text,
+                            formatter_class=MetavarTypeHelpFormatter,
+                            usage="%(prog)s [-h|--help] [-p|--pid] [-f|--homefolder] [<PROCESS_ID>]")
 
-    parser.add_argument("-p", "--pid", type=int,
-                        help="Show a list of all the parent process ids of the given <PROCESS_ID>.")
-    parser.add_argument("-f", "--homefolder", type=str,
-                        help="Show the path to the process owner‘s homefolder.")
+    pid_help_str = "Show a list of all the parent process ids of the given <PROCESS_ID>."
+    add_argument_to_parser(parser, '-p', '--pid', pid_help_str)
+
+    homefolder_help_str = "Show the path to the process owner‘s homefolder."
+    add_argument_to_parser(parser, '-f', '--homefolder', homefolder_help_str)
     return parser.parse_args()
 
 
-def get_user_name(child_pid):
-    user_name = subprocess.run(["ps", "-o", "user=", "-p", "%i" % child_pid], capture_output=True, text=True)
-    return user_name.stdout.replace("\n", "")
+def add_argument_to_parser(parser, short_flag, long_flag, help_str):
+    parser.add_argument(short_flag, long_flag, help=help_str)
 
 
-def get_given_process_info_as_list(pid: int) -> list[object]:
-    return [psutil.Process(pid)]
+def get_given_process_and_parent_info_as_list_of_dicts(pid: int) -> list[dict]:
+    current_pid = pid
+    process_info = []
+    while True:
+        process_info += [Process(current_pid).as_dict(attrs=['ppid', 'name', 'username', 'pid', 'cwd', 'status'])]
+        if current_pid == 1:
+            break
+        current_pid = process_info[-1]['ppid']
+    return process_info
 
 
-def get_all_parents_info_as_list(pid: int) -> list[object]:
-    return psutil.Process(pid).parents()
+def print_homefolder_of_process_owner(username):
+    if username == 'root':
+        return print("/root/")
+    else:
+        return print(Process(pid).cwd())
 
 
-args = define_parser_with_arguments()
+def format_and_print_PID_and_PPID_info(list_of_dicts):
+    for element in list_of_dicts:
+        print(f"PID: {element['pid']} | Name: {element['name']} | PPID: {element['ppid']} | "
+              f"User:{element['username']} | Status: {element['status']}")
+
 
 if __name__ == "__main__":
+    args = define_parser_with_arguments()
     pid = int(sys.argv[-1])
     if args.homefolder:
-        user = get_user_name(int(pid))
-        if user == "root":
-            print("/root")
-        else:
-            print(str(Path.home()) + "/")
+        print_homefolder_of_process_owner(Process(pid).username())
     if args.pid:
-        if not psutil.pid_exists(pid):
+        if not pid_exists(pid):
             print("Invalid <PROCESS-ID>")
         else:
-            process_info_list = get_given_process_info_as_list(pid) + get_all_parents_info_as_list(pid)
-            print(process_info_list)
+            proc_info_list_of_dicts = get_given_process_and_parent_info_as_list_of_dicts(pid)
+            format_and_print_PID_and_PPID_info(proc_info_list_of_dicts)
